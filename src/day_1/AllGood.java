9c492b9e6e9b8b3bc7a27bcf409cdcdcd8be1689
ogulle <oigulle@gmail.com>
2020-04-24 15:36:48 -0500

    add isko

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -3,6 +3,8 @@ package day_1;
 public class AllGood {
     public static void main(String[] args) {
 
+
+
 //I will see you soon
         //I hope everything is GOOD.
     }
--- src/day_1/isko.java
+++ src/day_1/isko.java
@@ -3,5 +3,7 @@ package day_1;
 public class isko {
     public static void main(String[] args) {
         System.out.println("what`s up?");
+
+        System.out.println("wow");
     }
 }
